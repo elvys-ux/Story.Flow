e81a8be958a4147e19f6b1926928ee46e80283cf
elvys-ux <elvysgraca.lx@ita.learnstudio.com>
2025-04-10 20:10:44 +0100

    Delete docs/js directory

--- docs/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ywyinndeowflitbqgsecc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aXlubmRhb3dsaWZicWNzYWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzgyNzAsImV4cCI6MjA1NzQ1NDI3MH0.7xH0Dpq_XYPFDJ2OmPFO-It3C5BIV_vWVLiNu7p1BFA';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
