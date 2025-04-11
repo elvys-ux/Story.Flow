12480be91f1a6c1c59baca8db60ec3bdb919db1a
elvys-ux <elvysgraca.lx@ita.learnstudio.com>
2025-04-11 15:18:34 +0100

    Update supabase.js

--- docs/js/supabase.js
+++ docs/js/supabase.js
@@ -1,8 +1,7 @@
-  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-
-// Use o URL e a Anon Key do seu projeto Supabase
-const supabaseUrl = 'https://ywiynndaowlifbqcsacc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aXlubmRhb3dsaWZicWNzYWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzgyNzAsImV4cCI6MjA1NzQ1NDI3MH0.7xH0Dpq_XYPFDJ2OmPFO-It3C5BIV_vWVLiNu7p1BFA';
+const supabaseUrl = "https://ywiynndaowlifbqcsacc.supabase.co";    // Ex: https://ywiynndaowlifbqcsacc.supabase.co
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aXlubmRhb3dsaWZicWNzYWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzgyNzAsImV4cCI6MjA1NzQ1NDI3MH0.7xH0Dpq_XYPFDJ2OmPFO-It3C5BIV_vWVLiNu7p1BFA";                // A "anon key" do seu projeto no painel do Supabase
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
