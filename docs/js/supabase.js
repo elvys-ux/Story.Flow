976411ccc5d766f42bac0da920bd9ef4b9edf1e1
elvys-ux <elvysgraca.lx@ita.learnstudio.com>
2025-04-12 14:50:24 +0100

    Update supabase.js

--- docs/js/supabase.js
+++ docs/js/supabase.js
@@ -1,20 +1,12 @@
-// Inicializa o Supabase corretamente
-const { createClient } = window.supabase; // <- Aqui está a correção!
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://ywiynndaowlifbqcsacc.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aXlubmRhb3dsaWZicWNzYWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzgyNzAsImV4cCI6MjA1NzQ1NDI3MH0.7xH0Dpq_XYPFDJ2OmPFO-It3C5BIV_vWVLiNu7p1BFA"
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = "https://ywiynndaowlifbqcsacc.supabase.co";   
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aXlubmRhb3dsaWZicWNzYWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzgyNzAsImV4cCI6MjA1NzQ1NDI3MH0.7xH0Dpq_XYPFDJ2OmPFO-It3C5BIV_vWVLiNu7p1BFA";                
 
-console.log("Supabase inicializado:", supabase);
-
-const { data, error } = await supabase.auth.signUp({
-  email: "exemplo@email.com",
-  password: "123456",
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    headers: { apikey: supabaseAnonKey }
+  }
 });
-
-if (data.user) {
-  await supabase.from("profiles").insert([
-    { id: data.user.id, username: "Nome do usuário" }
-  ]);
-}
