0aff82111275ea899ab1c9b37dbb57acfc14529b
elvys-ux <elvysgraca.lx@ita.learnstudio.com>
2025-04-12 13:17:05 +0100

    Update supabase.js

--- docs/js/supabase.js
+++ docs/js/supabase.js
@@ -5,4 +5,8 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = "https://ywiynndaowlifbqcsacc.supabase.co";   
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aXlubmRhb3dsaWZicWNzYWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4NzgyNzAsImV4cCI6MjA1NzQ1NDI3MH0.7xH0Dpq_XYPFDJ2OmPFO-It3C5BIV_vWVLiNu7p1BFA";                
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    headers: { apikey: supabaseAnonKey }
+  }
+});
